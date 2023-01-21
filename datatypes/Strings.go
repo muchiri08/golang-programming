a08d414101ef087379fba0cb48bfa5f0f463234c
muchiri08 <mbogokennedy08@gmail.com>
2023-01-21 16:15:06 +0300

    more on data types

--- datatypes/Strings.go
+++ datatypes/Strings.go
@@ -7,4 +7,13 @@ import "fmt"
 func main() {
 	fmt.Println("32132 x 42452 = ", 32132*42452)
 	fmt.Println((true && true) || (false && true) || !(false && false))
+
+	fmt.Println("Enter a number")
+	var input float64
+
+	fmt.Scanf("%f", &input)
+
+	output := input * 2
+
+	fmt.Println(output)
 }
