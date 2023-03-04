0ffc7c2fac0ecb45b254546242700d7826873284
muchiri08 <mbogokennedy08@gmail.com>
2023-03-04 13:35:29 +0300

    final commit

--- catProgram/main.go
+++ catProgram/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"os"
+	"strings"
 )
 
 func main() {
+	var tlc, twc, tcc int
 	for _, fname := range os.Args[1:] {
+		var lc, wc, cc int
+
 		file, err := os.Open(fname)
 
 		if err != nil {
@@ -15,10 +19,26 @@ func main() {
 			continue
 		}
 
-		if _, err := io.Copy(os.Stdout, file); err != nil {
-			fmt.Fprint(os.Stderr, err)
+		//if _, err := io.Copy(os.Stdout, file); err != nil {
+		//	fmt.Fprint(os.Stderr, err)
+		//}
+
+		scan := bufio.NewScanner(file)
+
+		for scan.Scan() {
+			s := scan.Text()
+
+			wc += len(strings.Fields(s))
+			cc += len(s)
+			lc++
 		}
+		fmt.Printf("%7d %7d %7d %s\n", lc, wc, cc, fname)
+		tlc += lc
+		twc += wc
+		tcc += cc
 
 		file.Close()
 	}
+
+	fmt.Printf("%7d %7d %7d totals\n", tlc, twc, tcc)
 }
