06727ff080dc31fff3bcfc10907ea8b517f5f197
muchiri08 <mbogokennedy08@gmail.com>
2023-05-04 20:51:10 +0300

    add myHandler func

--- htmlTemplate/main.go
+++ htmlTemplate/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
 type Entry struct {
 	Number int
 	Double int
@@ -8,6 +14,12 @@ type Entry struct {
 
 var DATA []Entry
 var tFile string
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Host: %s Path: %s\n", r.Host, r.URL.Path)
+	myT := template.Must(template.ParseGlob(tFile))
+	myT.ExecuteTemplate(w, tFile, DATA)
+}
 
 func main() {
 
