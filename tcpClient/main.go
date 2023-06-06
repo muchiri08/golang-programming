70c7183b8f5c82b807d479c35375283720baf7dc
muchiri08 <mbogokennedy08@gmail.com>
2023-06-06 22:03:05 +0300

    tcp client

---
+++ tcpClient/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Println("Please provide host:port.")
+		return
+	}
+
+	CONNECT := args[1]
+	conn, err := net.Dial("tcp", CONNECT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(conn, text+"\n")
+
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("->: " + message)
+		if strings.TrimSpace(string(text)) == "STOP" {
+			fmt.Println("TCP client exiting...")
+			return
+		}
+	}
+}
