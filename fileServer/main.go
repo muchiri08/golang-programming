1a7a73965a80bddc7993bf1b9678ef8445a53f43
muchiri08 <mbogokennedy08@gmail.com>
2023-06-19 22:28:49 +0300

    httprouter

---
+++ fileServer/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	router := httprouter.New()
+
+	//Mapping to methods is possible with HttpRouter
+	router.ServeFiles("/static/*filepath", http.Dir("/home/muchiri/static"))
+	log.Fatal(http.ListenAndServe(":8000", router))
+
+}
